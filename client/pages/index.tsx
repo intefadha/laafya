d70b9277ddcfa33023737048032d23247f83c3b2
Abderraouf Zine <rofazayn@gmail.com>
2021-07-11 20:04:27 +0100

    Install dependencies and run lerna.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import { addNumbers } from '@laafya/utils';
 export default function Home() {
   return (
     <div>
-      <h1>Result is: {addNumbers(1, 2)}</h1>
+      <h1>Result is: {addNumbers(3, 19)}</h1>
     </div>
   );
 }
