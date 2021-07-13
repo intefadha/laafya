a9487710324473807bfcb8a54a3a567a6ade9d31
Abderraouf Zine <rofazayn@gmail.com>
2021-07-13 01:21:04 +0100

    Clean-up the working directory.

--- client/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { addNumbers } from '@laafya/utils';
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Result is: {addNumbers(3, 19)}</h1>
-    </div>
-  );
-}
--- utils/src/index.ts
+++
@@ -1,7 +0,0 @@
-export function addNumbers(a: number, b: number): number {
-  if (typeof a === 'number' && typeof b === 'number') {
-    return a + b;
-  }
-
-  throw new Error('Please enter valid numbers!');
-}
